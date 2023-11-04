9a49ae0cc03de4f12f8ead1f22cdea027f7f2b0e
pc mélo <melodie.cochet@student.junia.com>
2023-11-04 22:48:53 +0100

    modal

--- library-site/src/components/AddAuthorModal/AddAuthorModal.tsx
+++ library-site/src/components/AddAuthorModal/AddAuthorModal.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 
-
 interface AddAuthorModalProps {
   isOpen: boolean;
   setIsModalOpen: (isOpen: boolean) => void;
@@ -13,24 +12,37 @@ const AddAuthorModal: React.FC<AddAuthorModalProps> = ({ isOpen, setIsModalOpen,
   const [lastName, setLastName] = useState('');
   const [photoUrl, setPhotoUrl] = useState('');
 
-  if (!isOpen) return null;
-
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    // Vous pouvez ici implémenter la logique pour vérifier ou générer l'id
+
     onAddAuthor({ id, firstName, lastName, photoUrl });
     setIsModalOpen(false);
   };
 
+  if (!isOpen) return null;
+
   return (
-    <div className="modal">
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} placeholder="Prénom" required />
-        <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} placeholder="Nom" required />
-        <input type="text" value={photoUrl} onChange={(e) => setPhotoUrl(e.target.value)} placeholder="URL de la photo" />
-        <button type="submit">Ajouter</button>
-        <button type="button" onClick={() => setIsModalOpen(false)}>Annuler</button>
-      </form>
+    <div className={`fixed inset-0 flex items-center justify-center z-50 ${isOpen ? '' : 'hidden'}`}>
+    <div className="modal bg-white rounded-lg p-6 max-w-lg mx-auto">
+        <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+            <input type="text" value={id} onChange={(e) => setId(e.target.value)} placeholder="ID" required className="w-full p-2 border border-gray-300 rounded"/>
+        </div>
+        <div className="mb-4">
+            <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} placeholder="Prénom" required className="w-full p-2 border border-gray-300 rounded"/>
+        </div>
+        <div className="mb-4">
+            <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} placeholder="Nom" required className="w-full p-2 border border-gray-300 rounded"/>
+        </div>
+        <div className="mb-4">
+            <input type="text" value={photoUrl} onChange={(e) => setPhotoUrl(e.target.value)} placeholder="URL de la photo" className="w-full p-2 border border-gray-300 rounded"/>
+        </div>
+        <div className="flex justify-end">
+            <button type="submit" className="bg-green-project text-white px-4 py-2 rounded-md mr-2">Ajouter</button>
+            <button type="button" onClick={() => setIsModalOpen(false)} className="text-red-500 px-4 py-2 rounded-md">Annuler</button>
+        </div>
+        </form>
+    </div>
     </div>
   );
 };
