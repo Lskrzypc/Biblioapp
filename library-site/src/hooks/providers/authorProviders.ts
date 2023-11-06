879ce7bce008d957a9f9c1e8847cfc9be8da079a
Sissoud <106033024+Sissoud@users.noreply.github.com>
2023-11-06 09:05:05 +0100

    Vos modifications

---
+++ library-site/src/hooks/providers/authorProviders.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { PlainAuthorModel } from '@/models';
+
+type UseListAuthorsProvider = {
+  authors: PlainAuthorModel[];
+  load: () => void;
+};
+
+export const useListAuthors = (): UseListAuthorsProvider => {
+    const [authors, setAuthors] = useState<PlainAuthorModel[]>([]);
+    
+    const fetchAuthors = (): void => {
+        axios
+        .get(`${process.env.NEXT_PUBLIC_API_URL}/authors`)
+        .then((data) => setAuthors(data.data))
+        .catch((err) => console.error(err));
+    };
+    
+    return { authors, load: fetchAuthors };
+};
+
+type AuthorProviders = {
+    useListAuthors: () => UseListAuthorsProvider;
+};
+
+export const useAuthorsProviders = (): AuthorProviders => ({
+    useListAuthors,
+});
