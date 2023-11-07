ba3b4ac6e3974f72923b8ff5a750522a4abdbfa2
Lskrzypc <louis.skrzypczak@student.junia.com>
2023-11-07 10:46:13 +0100

    :fix: forgot 2 files

---
+++ library-site/src/hooks/providers/bookByIdProviders.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { PlainBookModel } from '@/models';
+
+type UseBookByIdProvider = {
+    book: PlainBookModel;
+    load: (id: string) => void;
+};
+
+export const useBookById = (): UseBookByIdProvider => {
+    const [book, setBook] = useState<PlainBookModel>({} as PlainBookModel);
+
+    const fetchBook = (id: string): void => {
+        axios
+        .get(`${process.env.NEXT_PUBLIC_API_URL}/books/${id}`)
+        .then((data) => setBook(data.data))
+        .catch((err) => console.error(err));
+    };
+
+    return { book, load: fetchBook };
+}
+
+type BookByIdProviders = {
+    useBookById: () => UseBookByIdProvider;
+};
+
+export const useBookByIdProviders = (): BookByIdProviders => ({
+    useBookById,
+});
