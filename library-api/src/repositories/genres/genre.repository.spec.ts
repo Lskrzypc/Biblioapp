16fe4891fd56aec244810f55a0a44e8132ef6eac
Arthur LAFONT <arthur.lafont@student.junia.com>
2023-11-10 11:24:02 +0100

    feat(Genre): test on genre répository improved

--- library-api/src/repositories/genres/genre.repository.spec.ts
+++ library-api/src/repositories/genres/genre.repository.spec.ts
@@ -20,6 +20,27 @@ describe('GenreRepository', () => {
 
       expect(result).toEqual((genres));
     });
+    it("should return an empty array, if no genres are stored", async () => {
+        const dataSource = {
+            createEntityManager: jest.fn(),
+        } as unknown as DataSource;
+        const repository = new GenreRepository(dataSource);
+
+        const genres = [];
+
+        const findSpy = jest.spyOn(repository, 'find').mockResolvedValue(genres);
+
+        const result = await repository.getAllGenre();
+
+        expect(findSpy).toHaveBeenCalledTimes(1);
+        expect(findSpy).toHaveBeenCalledWith({
+            relations: {
+                bookGenres: true,
+            },
+        });
+
+        expect(result).toStrictEqual(genres);
+    })
   });
 
   describe('getGenreById', () => {
