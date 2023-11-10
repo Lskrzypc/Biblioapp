0a268979fbee7fc5a0de90d056c8652df450ec30
Arthur LAFONT <arthur.lafont@student.junia.com>
2023-11-10 11:23:49 +0100

    feat(Author): test on autho répository improved

--- library-api/src/repositories/authors/author.repository.spec.ts
+++ library-api/src/repositories/authors/author.repository.spec.ts
@@ -1,97 +1,118 @@
-import { DataSource } from 'typeorm';
-import { AuthorRepository } from './author.repository';
-import { authorFixture } from '../../fixtures';
-import { adaptAuthorEntityToPlainAuthorModel } from './author.utils';
+import {DataSource} from 'typeorm';
+import {AuthorRepository} from './author.repository';
+import {authorFixture} from '../../fixtures';
+import {adaptAuthorEntityToPlainAuthorModel} from './author.utils';
 
 describe('AuthorRepository', () => {
-  describe('getAllAuthors', () => {
-    it('should return all authors', async () => {
-      const dataSource = {
-        createEntityManager: jest.fn(),
-      } as unknown as DataSource;
-      const repository = new AuthorRepository(dataSource);
+    describe('getAllAuthors', () => {
+        it('should return all authors', async () => {
+            const dataSource = {
+                createEntityManager: jest.fn(),
+            } as unknown as DataSource;
+            const repository = new AuthorRepository(dataSource);
 
-      const authors = [authorFixture(), authorFixture(), authorFixture()];
+            const authors = [authorFixture(), authorFixture(), authorFixture()];
 
-      const findSpy = jest.spyOn(repository, 'find').mockResolvedValue(authors);
+            const findSpy = jest.spyOn(repository, 'find').mockResolvedValue(authors);
 
-      const result = await repository.getAllAuthors();
+            const result = await repository.getAllAuthors();
 
-      expect(findSpy).toHaveBeenCalledTimes(1);
-      expect(findSpy).toHaveBeenCalledWith({
-        relations: { books: true },
-      });
+            expect(findSpy).toHaveBeenCalledTimes(1);
+            expect(findSpy).toHaveBeenCalledWith({
+                relations: {books: true},
+            });
 
-      expect(result).toEqual(authors.map(adaptAuthorEntityToPlainAuthorModel));
+            expect(result).toEqual(authors.map(adaptAuthorEntityToPlainAuthorModel));
 
-      expect(true).toBeTruthy();
-      expect(null).toBeNull();
+            expect(true).toBeTruthy();
+            expect(null).toBeNull();
+        });
+        it("should return an empty array, if no authors are stored", async () => {
+            const dataSource = {
+                createEntityManager: jest.fn(),
+            } as unknown as DataSource;
+            const repository = new AuthorRepository(dataSource);
+
+            const authors = [];
+
+            const findSpy = jest.spyOn(repository, 'find').mockResolvedValue(authors);
+
+            const result = await repository.getAllAuthors();
+
+            expect(findSpy).toHaveBeenCalledTimes(1);
+            expect(findSpy).toHaveBeenCalledWith({
+                relations: {
+                    books: true,
+                },
+            });
+
+            expect(result).toStrictEqual(authors);
+        })
     });
-  });
 
-  describe('getAuthorById', () => {
-    it('should return found an author', async () => {
-      const dataSource = {
-        createEntityManager: jest.fn(),
-      } as unknown as DataSource;
-      const repository = new AuthorRepository(dataSource);
+    describe('getAuthorById', () => {
+        it('should return found an author', async () => {
+            const dataSource = {
+                createEntityManager: jest.fn(),
+            } as unknown as DataSource;
+            const repository = new AuthorRepository(dataSource);
 
-      const author = authorFixture();
+            const author = authorFixture();
 
-      const findOneSpy = jest
-        .spyOn(repository, 'findOne')
-        .mockResolvedValue(author);
+            const findOneSpy = jest
+                .spyOn(repository, 'findOne')
+                .mockResolvedValue(author);
 
-      const result = await repository.getAuthorById(author.id);
+            const result = await repository.getAuthorById(author.id);
 
-      expect(findOneSpy).toHaveBeenCalledTimes(1);
-      expect(findOneSpy).toHaveBeenCalledWith({ where: { id: author.id } });
+            expect(findOneSpy).toHaveBeenCalledTimes(1);
+            expect(findOneSpy).toHaveBeenCalledWith({where: {id: author.id}});
 
-      expect(result).toEqual(adaptAuthorEntityToPlainAuthorModel(author));
+            expect(result).toEqual(adaptAuthorEntityToPlainAuthorModel(author));
+        });
     });
-  });
 
-  describe('updateAuthor', () => {
-    it('should update an author', async () => {
-      const dataSource = {
-        createEntityManager: jest.fn(),
-      } as unknown as DataSource;
-      const repository = new AuthorRepository(dataSource);
+    describe('updateAuthor', () => {
+        it('should update an author', async () => {
+            const dataSource = {
+                createEntityManager: jest.fn(),
+            } as unknown as DataSource;
+            const repository = new AuthorRepository(dataSource);
 
-      const author = authorFixture();
+            const author = authorFixture();
 
-      const findOneSpy = jest
-        .spyOn(repository, 'findOne')
-        .mockResolvedValue(author);
+            const findOneSpy = jest
+                .spyOn(repository, 'findOne')
+                .mockResolvedValue(author);
 
-      const result = await repository.getAuthorById(author.id);
+            const result = await repository.getAuthorById(author.id);
 
-      expect(findOneSpy).toHaveBeenCalledTimes(1);
-      expect(findOneSpy).toHaveBeenCalledWith({ where: { id: author.id } });
+            expect(findOneSpy).toHaveBeenCalledTimes(1);
+            expect(findOneSpy).toHaveBeenCalledWith({where: {id: author.id}});
 
-      expect(result).toEqual(adaptAuthorEntityToPlainAuthorModel(author));
+            expect(result).toEqual(adaptAuthorEntityToPlainAuthorModel(author));
+        });
     });
-  });
 
-  describe('deleteById', () => {
-    it('should delete an author', async () => {
-      const dataSource = {
-        createEntityManager: jest.fn(),
-      } as unknown as DataSource;
-      const repository = new AuthorRepository(dataSource);
+    describe('deleteById', () => {
+        it('should delete an author', async () => {
+            const dataSource = {
+                createEntityManager: jest.fn(),
+            } as unknown as DataSource;
+            const repository = new AuthorRepository(dataSource);
 
-      const author = authorFixture();
+            const author = authorFixture();
 
-      const findOneSpy = jest
-        .spyOn(repository, 'findOne')
-        .mockResolvedValue(author);
+            const findOneSpy = jest
+                .spyOn(repository, 'findOne')
+                .mockResolvedValue(author);
 
-      const result = await repository.getAuthorById(author.id);
+            const result = await repository.getAuthorById(author.id);
 
-      expect(findOneSpy).toHaveBeenCalledTimes(1);
-      expect(findOneSpy).toHaveBeenCalledWith({ where: { id: author.id } });
+            expect(findOneSpy).toHaveBeenCalledTimes(1);
+            expect(findOneSpy).toHaveBeenCalledWith({where: {id: author.id}});
 
-      expect(result).toEqual(adaptAuthorEntityToPlainAuthorModel(author));
+            expect(result).toEqual(adaptAuthorEntityToPlainAuthorModel(author));
+        });
     });
-  });
 });
